e268130061e7844d1b3aac402c43d8ca5af2b3a9
elazar <elazarcoh@gmail.com>
2024-12-03 21:49:24 +0200

    update typings

--- src/pydom/types/html/html_select_element.py
+++ src/pydom/types/html/html_select_element.py
@@ -4,10 +4,10 @@ from pydom.types.html.html_element_props import HTMLElementProps
 
 class HTMLSelectElement(HTMLElementProps, total=False):
     auto_complete: Optional[str]
-    auto_focus: Optional[str]
+    auto_focus: Optional[bool]
     disabled: Optional[bool]
     form: Optional[str]
-    multiple: Optional[str]
+    multiple: Optional[bool]
     name: Optional[str]
-    required: Optional[str]
-    size: Optional[str]
+    required: Optional[bool]
+    size: Optional[int]
